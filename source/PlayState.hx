44a265e546e92d53772907a1c2804bb2f38f86aa
IdealistCat <sinconsistencia@gmail.com>
2025-01-29 16:17:44 -0600

    attempt at chance for the behavior

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,8 +1,10 @@
 package;
 
 import data.EnemyJson;
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxTimer;
 import haxe.Json;
 
 class PlayState extends FlxState
@@ -11,14 +13,18 @@ class PlayState extends FlxState
 
 	public var ENEMY_JSON:EnemyJson;
 	public var ENEMY_SPRITE:FlxSprite = new FlxSprite();
+	public var ENEMY_TIMER:FlxTimer;
 
 	override public function create()
 	{
 		ENEMY_JSON = Json.parse(FileManager.readFile(FileManager.getDataFile('enemies/$ENEMY.json')));
 		ENEMY_SPRITE.loadGraphic(FileManager.getImageFile('enemies/$ENEMY/${ENEMY_JSON.assets[ENEMY_JSON.behavior[0].array_index].file}'));
-
 		ENEMY_SPRITE.screenCenter();
 		add(ENEMY_SPRITE);
+
+		ENEMY_TIMER = new FlxTimer(new FlxTimerManager());
+
+		enemyTime();
 
 		super.create();
 	}
@@ -26,5 +32,22 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+	}
+	function enemyTime()
+	{
+		for (behavior in ENEMY_JSON.behavior)
+		{
+			if (FlxG.random.bool(behavior.chance))
+			{
+				ENEMY_SPRITE.loadGraphic(FileManager.getImageFile('enemies/$ENEMY/${ENEMY_JSON.assets[behavior.array_index].file}'));
+				trace(behavior.name);
+				break;
+			}
+		}
+
+		ENEMY_TIMER.start(FlxG.random.float(1, 2), _timer ->
+		{
+			enemyTime();
+		});
 	}
 }
