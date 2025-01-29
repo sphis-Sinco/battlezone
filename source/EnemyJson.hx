ca6957a3494c0d0fd66b17416fbff8b5de1e9231
IdealistCat <sinconsistencia@gmail.com>
2025-01-29 15:35:42 -0600

    Enemy Json typedef

---
+++ source/EnemyJson.hx
@@ -0,0 +1,18 @@
+typedef EnemyJson =
+{
+	var name:String;
+	var assets:Array<EnemyAssetJson>;
+	var behavior:Array<EnemyBehaviorJson>;
+}
+
+typedef EnemyAssetJson =
+{
+	var asset:String;
+	var file:String;
+}
+
+typedef EnemyBehaviorJson =
+{
+	var name:String;
+	var chance:Float;
+}
